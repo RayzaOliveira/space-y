fae92cc41c3bb196582b7021e12a18fed4dd4b6c
Rayza Oliveira <rayza.ocr@gmail.com>
2020-07-12 23:41:32 -0300

    Criando componente: Content

---
+++ src/components/Content/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './index.css'
+
+const Content = ({ children }) => {
+
+  return (
+    <div className="section-content-default">
+      {children}
+    </div>
+  )
+}
+
+export default Content
