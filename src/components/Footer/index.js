8844a2e756dc2fadea7a62b4205aa4d13f31e422
Rayza Oliveira <rayza.ocr@gmail.com>
2020-07-17 02:02:47 -0300

    Ajustando desing responsivo ao Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,25 +15,25 @@ const Footer = () => {
         <li>
           <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rayza-oliveira-costa-482658129/">
             <i className="fa fa-linkedin"/>
-            LinkedIn
+            <span>LinkedIn</span>
           </a>
         </li>
         <li>
           <a target="_blank" rel="noopener noreferrer" href="https://github.com/rayzaoliveira">
-          <i className="fa fa-github"/>
-            GitHub
+            <i className="fa fa-github"/>
+            <span>GitHub</span>
           </a>
         </li>
         <li>
           <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/oliveirarayza/">
-          <i className="fa fa-instagram"/>
-            Instagram
+            <i className="fa fa-instagram"/>
+            <span>Instagram</span>
           </a>
         </li>
         <li>
           <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/rayza_ocr">
-          <i className="fa fa-twitter"/>
-            Twitter
+            <i className="fa fa-twitter"/>
+            <span>Twitter</span>
           </a>
         </li>
       </ul>
