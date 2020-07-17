6414d6d46b10abbf220ae1fc2065c4b6651a57cb
Rayza Oliveira <rayza.ocr@gmail.com>
2020-07-17 02:05:01 -0300

    Ajustando reponsividade do componente Section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
+
 import './index.css'
 
-const Section = ({ children, id, color, alignment }) => {
+const Section = ({ children, id, imgBg, color, alignment,backgroundLess }) => {
 
   return (
     // interpolação de strings: inserindo uma variável no texto - id
     //parametrização do alinhamento: bottom-left, bottom-right, center-left, center-right  
     // parametrizando a cor do fundo
     <section  
-      className={`section-${id} default ${alignment}`} 
-      style={{ backgroundColor: color }}
+      className={`section-${id} default ${alignment} ${backgroundLess ?'background-less':''}`} 
+      style={{ 
+        backgroundColor: color, 
+        backgroundImage: `url(${imgBg})`,
+
+      }}
     >
       {children}
     </section>
