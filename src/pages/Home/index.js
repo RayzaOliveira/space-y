0cdfa21e65cb623ff42d7ce3de95497533fdb7cd
Rayza Oliveira <rayza.ocr@gmail.com>
2020-07-17 02:05:51 -0300

    Ajustando rotas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,12 @@ import LinkButton from '../../components/LinkButton'
 import Section from '../../components/Section'
 import Title from '../../components/Title'
 import Content from '../../components/Content'
-import Footer from '../../components/Footer'
-import Header from '../../components/Header' 
+
+import imgBg1 from '../../assets/images/starlinkstreak_desktop.png'
+import imgBg2 from '../../assets/images/gps3_launch_desktop_02.png'
+import imgBg3 from '../../assets/images/home_demo2.png'
+import imgBg5 from '../../assets/images/home_moon.png'
+
 
 import './index.css'
 
@@ -14,12 +18,12 @@ const Home = () => {
   return (
     <div id="page-home">
 
-      <Header/>
+      {/* <Header/> : foi tranferido para ser compartilhado entre todas as rotas */}
 
       {/* exportando para componets/Section: o id da sessão e cor do fundo */}
 
       {/* <section className="section-1 default" > */}
-      <Section id="1" alignment="bottom-left" color="#212529">
+      <Section id="1" alignment="bottom-left" imgBg={imgBg1}>
 
       {/* Componetizado para Content
         
@@ -29,7 +33,7 @@ const Home = () => {
           <Title tag="h4">UPCOMING LAUNCH</Title>
           <Title>STARLINK MISSION</Title>
 
-          <LinkButton route="https://www.google.com.br">
+          <LinkButton route="/page-model">
             UPDATE
           </LinkButton>
 
@@ -55,32 +59,32 @@ const Home = () => {
       {/* </section> */}
       </Section>
 
-      <Section id="2" alignment="bottom-right" color="#343a40">
+      <Section id="2" alignment="bottom-right" imgBg={imgBg2}>
         
         <Content>
 
           <Title>GPS III SPACE VEHICLE 03 MISSION</Title>
 
-          <LinkButton route="https://www.google.com.br">
+          <LinkButton route="https://youtu.be/6zr0nfG3Xy4/">
             REPLAY
           </LinkButton>
 
         </Content>
       </Section>
 
-      <Section id="3" alignment="bottom-left" color="#495057">
+      <Section id="3" alignment="bottom-left" imgBg={imgBg3}>
         <Content>
 
           <Title>RETURNING HUMAN SPACEFLIGHT TO THE UNITED STATES</Title>
 
-          <LinkButton route="https://www.google.com.br">
+          <LinkButton route="/page-model">
             LEARN MORE
           </LinkButton>
 
         </Content>
       </Section>
 
-      <Section id="4" alignment="center-left" color="#6c757d">
+      <Section id="4" alignment="center-left">
         <Content>
 
           <Title>CREW DRAGON DOCKING SIMULATOR</Title>
@@ -90,14 +94,21 @@ const Home = () => {
             Crew Dragon is designed to autonomously dock and undock with the International Space Station. However, the crew can take manual control of the spacecraft if necessary.
           </Title>
 
-          <LinkButton route="https://www.google.com.br">
+          <LinkButton route="https://iss-sim.spacex.com/">
             TRY IT
           </LinkButton>
 
         </Content>
+
+        <video className="video-simulator" width="50%" autoPlay loop muted>
+          <source src="https://www.spacex.com/media/iss_docking_sim.mp4" type="video/mp4" />
+          Your browser does not support HTML video.
+        </video>
+
+
       </Section>
 
-      <Section id="5" alignment="center-right" color="#adb5bd">
+      <Section id="5" alignment="center-right" imgBg={imgBg5}>
         <Content>
 
           <Title tag="h4">05/01/20</Title>
@@ -106,14 +117,14 @@ const Home = () => {
           NASA selected SpaceX to develop a lunar optimized Starship to transport crew between lunar orbit and the surface of the Moon as part of NASA’s Artemis program.
           </Title>
 
-          <LinkButton route="https://www.google.com.br">
+          <LinkButton route="/page-model">
             LEARN MORE
           </LinkButton>
 
         </Content>
       </Section>
-      {/* Rodapé do site */}
-      <Footer />
+      {/* Rodapé do site: foi tranferido para ser compartilhado entre todas as rotas 
+      <Footer /> */}
 
     </div>
   )
